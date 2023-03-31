fd0247f8ccae21aaf230176bdf565f1144e9f8b0
Sephi1412 <crisb1412@gmail.com>
2023-03-31 02:54:36 -0300

    Accordion Component: Implemented
    # TODO:
    1) Modify every component mounting process to do it in "#temporal-container". The main idea behind is that this invisible container is going to be able to hold every component until is assigned to a "parent".

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import * as $ from "jquery";
+import * as $ from 'jquery';
 import onChange from 'on-change';
 import { popper } from '@popperjs/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -21,92 +21,83 @@ import { TextInput } from './components/TextInput';
 import { Offcanvas } from './components/Offcanvas';
 import { Accordion } from './components/Accordion';
 
-
-const decisionModal = new DecisionModal({ menuId: "decision-modal" });
-const offcanvasTest = new Offcanvas({menuId: "offcanvas-test", title: "Test Offcanvas", body: ""})
+const decisionModal = new DecisionModal({ menuId: 'decision-modal' });
+const offcanvasTest = new Offcanvas({ menuId: 'offcanvas-test', title: 'Test Offcanvas', body: '' });
 OptionTabs();
 testEventListeners();
 
-
-
 const newObjective = {
-    currentValue: -1.0,
-}
+	currentValue: -1.0
+};
 
 function updateNewObjective(props) {
-    console.log("updateNewObjective(", props, ");")
-    newObjective["currentValue"] = props.value;
-    console.log("UPDATE NEW OBJECTIVE", newObjective);
+	console.log('updateNewObjective(', props, ');');
+	newObjective['currentValue'] = props.value;
+	console.log('UPDATE NEW OBJECTIVE', newObjective);
 }
 
 const rangeInput = new RangeInput({
-    id: "range-input",
-    props: {
-        min: 0,
-        max: 2,
-        currentValue: 1,
-        step: 0.5,
-    },
-    label: "Terrain Opacity",
-    callbacks: {
-        "change": updateNewObjective,
-        "click": updateNewObjective,
-    }
-    
-})
-
-
+	id: 'range-input',
+	props: {
+		min: 0,
+		max: 2,
+		currentValue: 1,
+		step: 0.5
+	},
+	label: 'Terrain Opacity',
+	callbacks: {
+		change: updateNewObjective,
+		click: updateNewObjective
+	}
+});
 
 const selectInput = new SelectInput({
-    id: "select-input",
-    
-    defaultValue: "Select an Option",
-    options: {
-        "key_seq": "Key Sequence",
-        "terrain_edit": "Terrain Modification",
-        "use_tool": "Use a Specific Tool"
-    },
-    label: "Exercise Type",
-    callbacks: {
-        "change": updateNewObjective,
-    }
-})
+	id: 'select-input',
+
+	defaultValue: 'Select an Option',
+	options: {
+		key_seq: 'Key Sequence',
+		terrain_edit: 'Terrain Modification',
+		use_tool: 'Use a Specific Tool'
+	},
+	label: 'Exercise Type',
+	callbacks: {
+		change: updateNewObjective
+	}
+});
 
 const checkInput = new Checkbox({
-    id: "check-input",
-    containerID: "input-test",
-    label: "Exercise Type",
-    callbacks: {
-        "change": updateNewObjective,
-    },
-})
+	id: 'check-input',
+	containerID: 'input-test',
+	label: 'Exercise Type',
+	callbacks: {
+		change: updateNewObjective
+	}
+});
 
 const textArea = new TextAreaInput({
-    id: "text-area",
-    containerID: "input-test",
-    label: "Description",
-    callbacks: {
-        "input" : updateNewObjective,
-    }
-})
+	id: 'text-area',
+	containerID: 'input-test',
+	label: 'Description',
+	callbacks: {
+		input: updateNewObjective
+	}
+});
 
 const textInput = new TextInput({
-    id: "text-input",
-    label: "Objective Title",
-    callbacks: {
-        "input" : updateNewObjective,
-    },
-    props: {
-        placeholder: "Insert here the name of the objetive"
-    }
-})
+	id: 'text-input',
+	label: 'Objective Title',
+	callbacks: {
+		input: updateNewObjective
+	},
+	props: {
+		placeholder: 'Insert here the name of the objetive'
+	}
+});
 
-
-
-const offcanvasButton = document.getElementById("btn-test-offcanvas");
-offcanvasButton.addEventListener("click", () => {
-    
-    offcanvasTest.handleVisibility();
+const offcanvasButton = document.getElementById('btn-test-offcanvas');
+offcanvasButton.addEventListener('click', () => {
+	offcanvasTest.handleVisibility();
 });
 
 offcanvasTest.addContentToBody(rangeInput);
@@ -114,11 +105,6 @@ offcanvasTest.addContentToBody(textInput);
 offcanvasTest.addContentToBody(textArea);
 offcanvasTest.addContentToBody(checkInput);
 
-const testAccordion = new Accordion({id: "test-accordion"});
-testAccordion.addChildren();
-offcanvasTest.addContentToBody(testAccordion)
-testAccordion.addChildren();
-testAccordion.addChildren();
-testAccordion.addChildren();
-testAccordion.addChildren();
-testAccordion.addChildren();
+const testAccordion = new Accordion({ id: 'test-accordion' });
+offcanvasTest.addContentToBody(testAccordion);
+testAccordion.addChildren({ itemLabel: 'Objective N°1' , itemContent: selectInput.el});
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,84 +1,77 @@
-import { el, mount, setChildren } from "redom";
+import { el, mount, setChildren } from 'redom';
 import onChange from 'on-change';
 
 class AccordionItem {
-    constructor({ parent, parentID, index }) {
-        this.id = `${parentID}-item-${index}`;
-        this.parent = parent;
-        this.headerID = `${this.id}-header`;
-        this.bodyID = `${this.id}-body`;
+	constructor({ label = '', content, parent, parentID, index }) {
+		this.id = `${parentID}-item-${index}`;
+		this.parent = parent;
+		this.headerID = `${this.id}-header`;
+		this.bodyID = `${this.id}-body`;
+        this.label = label;
+        this.content = content;
 
-        this._init()
-    }
+		this._init();
+	}
 
-    _init() {
-        this.button = el(`button.accordion-button#${this.id}-button`, {
-            type: "button",
-            "data-bs-toggle": "collapse",
-            "data-bs-target": `#${this.bodyID}`,
-            "aria-expanded": "true",
-            "aria-controls": `${this.bodyID}`,
-            textContent: `Test Accordion: ${this.id}`
-        })
+	_init() {
+		this.button = el(`button.accordion-button#${this.id}-button`, {
+			type: 'button',
+			'data-bs-toggle': 'collapse',
+			'data-bs-target': `#${this.bodyID}`,
+			'aria-expanded': 'true',
+			'aria-controls': `${this.bodyID}`,
+			textContent: this.label
+		});
 
-        this.body = el(`div.accordion-collapse collapse#${this.bodyID}`, { "data-bs-parent": `${this.parentID}`, "aria-labelledby": `${this.headerID}`, }, [
-            el("div.accordion-body", "{Children msg}")
-        ]);
+		this.body = el(
+			`div.accordion-collapse collapse#${this.bodyID}`,
+			{ 'data-bs-parent': `${this.parentID}`, 'aria-labelledby': `${this.headerID}` },
+			[ el('div.accordion-body', [ this.content ]) ]
+		);
 
-        this.header = el(`h2.accordion-header#${this.headerID}`, [
-            this.button
-        ])
+		this.header = el(`h2.accordion-header#${this.headerID}`, [ this.button ]);
+		this.el = el(`div.accordion-item#${this.id}`, [ this.header, this.body ]);
 
-        this.el = el(`div.accordion-item#${this.id}`, [
-            this.header,
-            this.body
-        ])
+		mount(this.parent, this.el);
 
-        // this.bsCollapse = new bootstrap.Collapse(`#${this.id}-button`)
-        // console.log(this.bsCollapse);
-        // this.setEventListeners();
-
-    }
-
-    setEventListeners() {
-        this.bsCollapse = new bootstrap.Collapse(`#${this.bodyID}`, { toggle: true, parent: this.parent });
-        this.button.addEventListener("click", event => {
-            $(`#${this.bodyID}`).collapse({ "toggle": true, 'parent': this.parent });
-
-        });
-    }
+		this.bsCollapse = new bootstrap.Collapse(`#${this.bodyID}`, { toggle: false, parent: this.parent });
+		// this.setEventListeners();
+	}
 }
 
-
 export class Accordion {
-    constructor({ id }) {
-        this.id = id;
-        // this.children = {};
-        this.n_childrens = 0;
-        this._init();
-    }
+	constructor({ id }) {
+		this.id = id;
+		// this.children = {};
+		this.n_childrens = 0;
+		this._init();
+	}
 
-    _init() {
-        if (this.id) {
-            this.el = el(`div.accordion container#${this.id}`)
-        }
+	_init() {
+		if (this.id) {
+			this.el = el(`div.accordion container#${this.id}`);
+			mount(document.getElementById('temporal-container'), this.el);
+		} else console.error('An ID must be defined for the accordion');
+	}
 
-        else
-            console.error("An ID must be defined for the accordion");
-    }
+	setParent(parent) {
+		mount(parent, this.el);
+	}
 
-    setParent(parent) {
-        mount(parent, this.el);
-    }
-
-    addChildren() {
-        const children = new AccordionItem({ parent: this.el, parentID: this.id, index: this.n_childrens })
-        children.button.addEventListener("click", event => {
-            this.activeButton = children.button;
-            console.log(this.activeButton.id);
-        })
-        this.el.appendChild(children.el);
-        this.n_childrens += 1;
-        // console.log(children.body);
-    }
+	addChildren({ itemLabel, itemContent }) {
+		const children = new AccordionItem({
+			label: itemLabel,
+			content: itemContent,
+			parent: this.el,
+			parentID: this.id,
+			index: this.n_childrens
+		});
+		children.button.addEventListener('click', (event) => {
+			this.activeButton = children.button;
+			console.log(this.activeButton.id);
+		});
+		this.el.appendChild(children.el);
+		this.n_childrens += 1;
+		// console.log(children.body);
+	}
 }
